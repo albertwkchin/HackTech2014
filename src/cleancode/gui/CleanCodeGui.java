6702b6327c33d69ee83426a58eb92de56fef5121
Albert Chin <chinaw@usc.edu>
2014-01-25 02:31:46 -0800

    Basic initial commit with main gui window class + functional parent class

---
+++ src/cleancode/gui/CleanCodeGui.java
@@ -0,0 +1,10 @@
+package cleancode.gui;
+
+public class CleanCodeGui {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/functionalcomponents/TextParser.java
@@ -0,0 +1,13 @@
+package functionalcomponents;
+
+
+/* GENERAL TEXTPARSER SUPERCLASS */
+// Every supported language will have its own subclass that inherits from this class
+public class TextParser {
+	
+	public void parseFile(String fileName) { }
+	
+	public void importLibrary(String language) { }
+	
+
+}
